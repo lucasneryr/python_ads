9ec1e5e50de8bf4910a4a8723bdec7d315ab3153
Lucas Reis <nerylucasreis@gmail.com>
2026-05-06 19:29:02 +0000

    dois exercicios de calculo fatorial e conversor de temperatura

---
+++ calculo_fatorial/calculo_fatorial.py
@@ -0,0 +1,7 @@
+import math
+
+fatorial_usuario = int(input("Informe um número de 1 a 10: "))
+
+fatorial_resultado = math.factorial(fatorial_usuario)
+
+print(f"O resultado do número {fatorial_usuario} fatorado será: {fatorial_resultado}")
---
+++ conversor_temperatura/conversor_temperatura.py
@@ -0,0 +1,5 @@
+temperatura_usuario = float(input("Digite a temperatura que deseja converter: ")) #solicitar a temperatura do usuário e já converter em float
+
+temperatura_convertida = (temperatura_usuario * 9 / 5) + 32 #converter a temperatura do usuário em fahrenheit
+
+print(f"A temperatura convertida é {temperatura_convertida} graus Fahrenheit.")
